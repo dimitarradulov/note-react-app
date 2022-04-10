085d0a9fa79abf604e41678f6c1e61b41e0bf91a
Dimitar Radulov <dimitaradulov@gmail.com>
2022-04-10 14:49:40 +0300

    Add moment library for dates

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import { useState } from 'react';
+import moment from 'moment';
+import Moment from 'react-moment';
 
 import Header from './components/Header/Header';
 import NewNote from './components/NewNote/NewNote';
@@ -8,13 +10,13 @@ const DUMMY_NOTES = [
   {
     title: 'Home WiFi Password',
     content: 'The password for the wifi at home: ILovePizza332$Aligator',
-    date: new Date(),
+    date: <Moment format="YYYY/MM/DD">{moment()}</Moment>,
   },
   {
     title: 'My Blog Article I Have To Post',
     content:
       'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
-    date: new Date(),
+    date: <Moment format="YYYY/MM/DD">{moment()}</Moment>,
   },
 ];
 
