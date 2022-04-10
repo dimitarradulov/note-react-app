4d894835fcbc248c4ab85166ab5fe8d1e78fb015
Dimitar Radulov <dimitaradulov@gmail.com>
2022-04-10 13:24:47 +0300

    Remove button component

--- src/components/UI/Button.js
+++
@@ -1,8 +0,0 @@
-import './Button.css';
-
-const Button = (props) => {
-  const classes = 'btn ' + props.classes;
-  return <button className={classes}>{props.children}</button>;
-};
-
-export default Button;
