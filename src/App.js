a4de7927a6530674b1263dd298931c277ed652d2
Dimitar Radulov <dimitaradulov@gmail.com>
2022-04-11 22:46:46 +0300

    Add react modal functionality and style

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import moment from 'moment';
 import Moment from 'react-moment';
+import ReactModal from 'react-modal';
 
 import Header from './components/Header/Header';
 import NewNote from './components/NewNote/NewNote';
 import Notes from './components/Notes/Notes';
-import Modal from './components/Modal/Modal';
 
 const DUMMY_NOTES = [
   {
@@ -61,9 +61,55 @@ const App = () => {
         onDelete={deleteNoteData}
         onViewDetails={viewDetailsHandler}
       />
-      {isViewingDetails.viewing && (
-        <Modal onCloseModal={closeModalHandler} note={isViewingDetails.note} />
-      )}
+      <ReactModal
+        isOpen={isViewingDetails.viewing}
+        onRequestClose={closeModalHandler}
+        style={{
+          overlay: {
+            backgroundColor: 'rgba(0, 0, 0, 0.65)',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            paddingInline: '10px',
+          },
+          content: {
+            width: '800px',
+            maxWidth: '100%',
+            position: 'relative',
+            top: '0',
+            left: '0',
+            right: '0',
+            bottom: '0',
+          },
+        }}
+      >
+        <h3 style={{ marginBottom: '15px' }}>{isViewingDetails.note.title}</h3>
+        <p style={{ lineHeight: '1.5' }}>{isViewingDetails.note.content}</p>
+        <button
+          onClick={closeModalHandler}
+          style={{
+            position: 'absolute',
+            right: '10px',
+            top: '10px',
+            border: 'none',
+            background: 'transparent',
+            cursor: 'pointer',
+          }}
+        >
+          <svg
+            width="20"
+            height="20"
+            viewBox="0 0 20 20"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M10 0C4.47 0 0 4.47 0 10C0 15.53 4.47 20 10 20C15.53 20 20 15.53 20 10C20 4.47 15.53 0 10 0ZM15 13.59L13.59 15L10 11.41L6.41 15L5 13.59L8.59 10L5 6.41L6.41 5L10 8.59L13.59 5L15 6.41L11.41 10L15 13.59Z"
+              fill="#111113"
+            />
+          </svg>
+        </button>
+      </ReactModal>
     </div>
   );
 };
