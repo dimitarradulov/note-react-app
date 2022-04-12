364d42d868d0adbab2e386ce5065edf7238784c3
Dimitar Radulov <dimitaradulov@gmail.com>
2022-04-12 12:19:20 +0300

    Add to useEffect dependencies

--- src/components/util/useListenForOutsideClicks.js
+++ src/components/util/useListenForOutsideClicks.js
@@ -16,7 +16,7 @@ const useListenForOutsideClicks = (ref, isOpenSetter) => {
       // Unbind the event listener on clean up
       document.removeEventListener('mousedown', handleClickOutside);
     };
-  }, [ref]);
+  }, [ref, isOpenSetter]);
 };
 
 export default useListenForOutsideClicks;
